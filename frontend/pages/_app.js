4412c13f4866e339adc2049785a6ffa642bd3959
Andres Filomeno <alvaro.andres1996@hotmail.com>
2022-08-24 08:22:05 -0500

    Initial Proyect (Video 7)

---
+++ frontend/pages/_app.js
@@ -0,0 +1,16 @@
+import Page from '../components/Page';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+export default function myApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
